35b7046c528ffe5c10a063c39e5804fc1f82a23e
Yashabangaram <yashabangaram@gmail.com>
2026-01-02 20:42:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,12 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.title("DIY Farmer’s Eye 🌱")
-st.write("Upload a plant image to detect water stress.")
+st.cache_resource.clear()
 
-model = tf.keras.models.load_model("plant_stress_model (1).h5")
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("FINAL_plant_stress_model_3class.h5")
+
+model = load_model()
 
 class_names = ["Healthy", "Mild Stress", "Severe Stress"]
-
-uploaded_file = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
-
-if uploaded_file:
-    img = Image.open(uploaded_file).convert("RGB")
-    img = img.resize((224, 224))
-
-    img_array = np.array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
-
-    predictions = model.predict(img_array)
-    predicted_class = class_names[np.argmax(predictions)]
-    confidence = np.max(predictions)
-
-    st.image(img, caption="Uploaded Image", use_column_width=True)
-    st.write(f"**Prediction:** {predicted_class}")
-    st.write(f"**Confidence:** {confidence:.2%}")
