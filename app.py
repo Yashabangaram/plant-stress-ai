76c0e385717f34acca7af08b565dc2f6ac0a2d07
Yashabangaram <yashabangaram@gmail.com>
2026-01-03 12:36:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,23 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.title("Plant Water Stress Detector")
+st.title("AI Plant Water Stress Detector")
 
-# Load model
-model = tf.keras.models.load_model("FINAL_plant_stress_model_3class.h5")
+model = tf.keras.models.load_model("plant_stress_model.keras")
 
 class_names = ["Healthy", "Mild Stress", "Severe Stress"]
 
 uploaded_file = st.file_uploader("Upload a plant image", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not None:
+if uploaded_file:
     img = Image.open(uploaded_file).convert("RGB")
     img = img.resize((224, 224))
-    
-    img_array = np.array(img)
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(np.array(img), axis=0)
 
-    predictions = model.predict(img_array)
-    predicted_class = class_names[np.argmax(predictions)]
-    confidence = np.max(predictions)
+    preds = model.predict(img_array)
+    label = class_names[np.argmax(preds)]
+    confidence = np.max(preds)
 
     st.image(img, caption="Uploaded Image", use_column_width=True)
-    st.write(f"Prediction: **{predicted_class}**")
+    st.write(f"Prediction: **{label}**")
     st.write(f"Confidence: **{confidence:.2f}**")
